3d92b8e0cd11bdff19230ef89e3a17472c04810e
Arunkumar <arunkumarb3509@gmail.com>
2025-05-14 22:31:48 +0530

    updated product-form-components

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -10,24 +10,45 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductFormComponent implements OnInit {
 
-form: FormGroup;
-submittedData: { name: string; minQty: number }[] = [];
+form!: FormGroup;
+  productId: number | null = null;
 
-constructor(private fb: FormBuilder) {
-  this.form = this.fb.group({
-    name: ['', Validators.required],
-    minQty: ['', [Validators.required, Validators.min(1)]]
-  });
-}
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
-ngOnInit(): void {
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      minQty: [0, [Validators.required, Validators.min(1)]]
+    });
+
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.productId = idParam ? Number(idParam) : null;
+
+    if (this.productId) {
+      this.productService.getById(this.productId).subscribe(product => {
+        this.form.patchValue(product);
+      });
+    }
   }
-  
-onSubmit() {
-  if (this.form.valid) {
-    this.submittedData.push(this.form.value);
-    this.form.reset();
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    const data = this.form.value;
+
+    const req = this.productId
+      ? this.productService.update(this.productId, data)
+      : this.productService.create(data);
+
+    req.subscribe({
+      next: () => this.router.navigate(['/products']),
+      error: (err) => console.error('Error saving product', err)
+    });
   }
-}
 
 }
